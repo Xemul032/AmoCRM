f46ceaeb9d7f0dcbdc1708a119724ea3fbc0201c
Xemul032 <ritavsvitere@gmail.com>
2025-05-26 17:03:13 +0300

    Add files via upload

---
+++ AmoLoader-3.0.user.js
@@ -0,0 +1,52 @@
+// ==UserScript==
+// @name AmoLoader
+// @namespace http://tampermonkey.net/
+// @version 1.0
+// @description Загружает и выполняет скрипт с GitHub
+// @author Рустам Кандеев, Александр Щёкин
+// @match https://cplink.amocrm.ru/*
+// @icon https://cplink.amocrm.ru/frontend/images/interface/meta/Icon-72.png
+// @grant        GM_xmlhttpRequest
+// @grant        GM_addStyle
+// @connect      sheets.googleapis.com
+// @connect      docs.google.com
+// @connect      script.google.com
+// @connect      script.googleusercontent.com
+// @connect      raw.githubusercontent.com
+// @connect      api.ipify.org
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // URL основного скрипта на GitHub
+    const scriptUrl = 'https://raw.githubusercontent.com/Xemul032/AmoCRM/refs/heads/main/amoCustomJS.js';
+
+    // Функция для загрузки и исполнения скрипта
+    function loadScript(url) {
+    GM_xmlhttpRequest({
+    method: 'GET',
+    url: url,
+    onload: function(response) {
+    // Проверяем, успешно ли загружен скрипт
+    if (response.status === 200) {
+    try {
+    // Выполняем загруженный скрипт
+    eval(response.responseText);
+    } catch (e) {
+    console.error('Ошибка при выполнении скрипта:', e);
+    }
+    } else {
+    console.error('Ошибка загрузки скрипта: HTTP статус', response.status);
+    }
+    },
+    onerror: function(err) {
+    console.error('Ошибка при запросе:', err);
+    }
+    });
+    }
+
+    // Загружаем и запускаем скрипт
+    loadScript(scriptUrl);
+
+    })();
